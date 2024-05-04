bac980a935a9afd35c4d5151b3858b3b38284f2e
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-05-04 22:47:14 +0900

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use serenity::model::channel::Message;
 use serenity::prelude::{Client, Context, EventHandler};
 use serenity::async_trait;
@@ -7,12 +8,14 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
 	async fn message(&self, ctx: Context, msg: Message){
-		msg.channel_id.say(&ctx, "msg.content);
+		msg.channel_id.say(&ctx, msg.content);
 	}
-	
+ }
+ 
 #[tokio::main]
 async fn main() {
-    let mut client = Client::builder(env).event_handler(Handler).await?;
+    let token = env::var("TOKEN").expect("");
+    let mut client = Client::builder(token).event_handler(Handler).await?;
     if let Err(why) = client.start().await {
         println!("問題→{why:?}");
     }
