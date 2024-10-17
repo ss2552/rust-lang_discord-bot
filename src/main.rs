d121323519f1a38170b1601395b29ce601863508
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-10-17 23:11:25 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use std::env;
-use serenity::prelude::{Client};
+
+pub extern crate futures;
+pub extern crate url;
+
+use self::client::builder::ClientBuilder;
+use self::result::WebSocketResult;
  
 #[tokio::main]
 async fn main() {
-    let mut args = std::env::args.collect();
-    let mut client = Client::builder(args[1]).event_handler(Handler).await?;
-    client.start().await?;
+ let mut args:Vec<String> = std::env::args().collect();
+ 
+ let mut runtime = tokio::runtime::current_thread::Builder::new()
+		.build()
+		.unwrap();
+ 
+ let runner = ClientBuilder::new("wss://gateway.discord.com/api/?v=v6&encoding=json")
+  .unwrap()
+  .async_connect_secure()
+  let _ = runtime.block_on(runner).unwrap();
 }
