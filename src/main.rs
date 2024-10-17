54a2de52a8998a6db7869f77e118bd2afad6d354
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-10-18 00:12:41 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
 use std::env;
 
-pub extern crate futures;
-pub extern crate url;
+extern crate futures;
+extern crate tokio;
+extern crate websocket;
 
-use self::client::builder::ClientBuilder;
-use self::result::WebSocketResult;
- 
-#[tokio::main]
+use websocket::{ClientBuilder, OwnedMessage};
+
+const base_url = "wss://gateway.discord.com/api/?v=v6";
+const ws_url = base_url + "&encoding=json";
+
+
+let mut args:Vec<String> = std::env::args().collect();
+
+const TOKEN = args[1];
+async fn message(){
+
+}
+
+async fn close(){
+	
+}
+
+async fn open(s){
+	await s.send("{op: 2, d: {token: 'Bot "+TOKEN+"', properties: {$os: 'linux', $device: 'chrome'}}}")
+}
+
+async fn connect(){
+	let client = ClientBuilder::new(ws_url)
+		.unwrap()
+		.async_connect_secure()
+		.and_then(|(s, _)| open(s));
+	let _ = runtime.block_on(client).unwrap();
+	return 
+}
+
 async fn main() {
- let mut args:Vec<String> = std::env::args().collect();
  
- let mut runtime = tokio::runtime::current_thread::Builder::new()
+	let mut runtime = tokio::runtime::current_thread::Builder::new()
 		.build()
 		.unwrap();
- 
- let runner = ClientBuilder::new("wss://gateway.discord.com/api/?v=v6&encoding=json")
-  .unwrap()
-  .async_connect_secure()
-  let _ = runtime.block_on(runner).unwrap();
+ 	await connect();
+
 }
